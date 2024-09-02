0ac18cecf7eeca93b11558b9bcd0b7052db7d4c7
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-09-01 21:57:32 -0600

    Update AgeCalculator.java
    import libraries and configure settings for JFrame

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -1,8 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class AgeCalculator extends JFrame {
 
     private JPanel mainPanel;
 
     public AgeCalculator() {
+        setTitle("Age Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(500, 200)); // Set minimum size
+        setSize(500, 200);
+        setLocationRelativeTo(null); // Center the window
+
         add(mainPanel);
         setVisible(true);
     }
